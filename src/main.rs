da1d1742af303cf123e955614f3525a4db92f49e
JamesTann <jt.21@dartmouth.edu>
2020-06-19 22:39:22 -0400

    Argument parsing done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::env;
+use std::fs;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Error reading file.");
 }
+
+struct Config {
+    query: String,
+    filename: String
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+
+        if args.len() < 3 {
+            return Err("Not enough input arguments.");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+
